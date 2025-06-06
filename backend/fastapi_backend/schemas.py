ed94e95515d1136e72c97bea04edb98b9b1c2a91
fifi <djgamez1222@gmail.com>
2025-06-06 20:45:48 +0200

    Creating models and schemas for database

---
+++ backend/fastapi_backend/schemas.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+
+class ItemBase(BaseModel):
+    name: str
+    quantity: int
+
+class ItemCreate(ItemBase):
+    pass
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+class UserBase(BaseModel):
+    username: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
